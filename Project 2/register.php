ae8dbee2f49980e2057a0a3bcc3ea61e81de7670
Tek Acharya <60335154+tacharya2@users.noreply.github.com>
2020-11-05 15:21:16 -0500

    Update register.php
    updated register.php as well

--- Project 2/register.php
+++ Project 2/register.php
@@ -1,44 +1,50 @@
 <?php
 
- $file1 = "visitors.txt";
-  $current_content1 = file_get_contents($file1);
+$file1 = "visitors.txt";
+$current_content1 = file_get_contents($file1);
 
-  ?>
+?>
 <pre>
 <!-- connect this to  login.html-->
-header(Location: )
+header(Location:  )
 </pre>
-  <?php
-  $current_content1 .= $_POST["name"] . "," . $_POST["username"] . "," .
-    $_POST["psw"]  . "," . $_POST["cpsw"]. "\n";
-  file_put_contents($file1, $current_content1);
-  ?>
-<?php 
-$visitors = file("visitors.txt");
-/* Find and get user */
-$user_info_line = '';
-for ($i = 0; $i < count($visitors); $i++) {
-    $user_info_line = strstr($visitors[$i], $_GET["name"]);
-    if ($user_info_line !== FALSE) {
-        break;
-    }
-}
+<?php
+$current_content1 .= $_POST["name"] . "," . $_POST["username"] . "," .
+  $_POST["psw"]  . "," . $_POST["cpsw"] . "\n";
+file_put_contents($file1, $current_content1);
+?>
+<?php
+// $visitors = file("visitors.txt");
+// /* Find and get user */
+// $user_info_line = '';
+// for ($i = 0; $i < count($visitors); $i++) {
+//   $user_info_line = strstr($visitors[$i], $_POST["name"]);
+//   if ($user_info_line !== FALSE) {
+//     break;
+//   }
+// }
 
-$user_info = explode(",", $user_info_line);
-
-$username = $user_info[1];
+// $user_info = explode(",", $user_info_line);
+// $username = $user_info[1];
+// $password = $user_info[2];
 ?>
 
 
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-  </head>
-  <body>
-    <a href="signup.html">Sign up</a>
-    
-  </body>
-  </html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <!-- <a href="signup.html">Sign up</a> -->
+  <a href="signup.html" onclick="signup()"> Sign In</a>
+
+</body>
+
+</html>
