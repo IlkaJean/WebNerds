7a149c3b6c2d2a0b88e09bc915c7fa58dc3c5760
Ilka Jean <57559023+IlkaJean@users.noreply.github.com>
2020-12-01 21:00:49 -0500

    Update fifteen.js

--- Project 3/fifteen.js
+++ Project 3/fifteen.js
@@ -1,113 +1,275 @@
-var item;
-var X;
-var Y;
+var moves = 0; 
+var table, rows, columns, textMoves, arrayForBoard; 
 
-window.onload = function() {
-	item = document.getElementById('puzzles').getElementsByTagName('div');
 
-	for (var i = 0; i < item.length; i++) {
-		item[i].style.backgroundImage = "url('background.jpg')";
-		item[i].style.left = (i % 4) * 100 + 'px';
-		item[i].style.top = parseInt(i / 4) * 100 + 'px';
-		item[i].style.backgroundPosition =
-			'-' + item[i].style.left + ' ' + '-' + item[i].style.top;
-		item[i].onmouseover = function() {
-			if (moved(parseInt(this.innerHTML))) {
-				this.style.border = '3px solid red';
-				this.style.color = 'green';
-				this.style.textDecoration = 'underline';
-			}
-		};
+function checkAnswer(){
+  var ans = prompt("Choose background image: Among Us, Fortnite, Minecraft, Mario");
+  if(ans =='Among Us'){
+    document.getElementById('puzzle').style.backgroundImage="url(./amongus.png)";
+  }
+  else if (ans=='Fortnite'){
+    document.getElementById('puzzle').style.backgroundImage="url(./fortnite.png)";
+  }
+  else if(ans=='Minecraft'){
+    document.getElementById('puzzle').style.backgroundImage="url(./mine.png)";
+  }
+  else if (ans=='Mario'){
+    document.getElementById('puzzle').style.backgroundImage="url(./mario.png)";
+  }
 
-		item[i].onmouseout = function() {
-			this.style.border = '2px solid black';
-			this.style.color = 'black';
-			this.style.textDecoration = 'none';
-		};
-
-		item[i].onclick = function() {
-			if (moved(parseInt(this.innerHTML))) {
-				swap_locations(this.innerHTML - 1);
-				return;
-			}
-		};
-	}
-
-	var shuffle = document.getElementById('suffle');
-	Y = '300px';
-	X = '300px';
-	shuffle.onclick = function() {
-		for (var i = 0; i < 300; i++) {
-			var random = parseInt(Math.random() * 100) % 4;
-
-			var temp = move(Y, X, random);
-
-			if (temp != -1) {
-				swap_locations(temp);
-			}
-		}
-	};
-};
-
-function move(x, y, random) {
-	var x = parseInt(x);
-	var y = parseInt(y);
-	if (x > 0 && random == 0) {
-		for (var i = 0; i < item.length; i++) {
-			if (
-				parseInt(item[i].style.left) + 100 == x &&
-				parseInt(item[i].style.top) == y
-			) {
-				return i;
-			}
-		}
-	} else if (x < 300 && random == 1) {
-		for (var i = 0; i < item.length; i++) {
-			if (
-				parseInt(item[i].style.left) - 100 == x &&
-				parseInt(item[i].style.top) == y
-			) {
-				return i;
-			}
-		}
-	} else if (y > 0 && random == 2) {
-		for (var i = 0; i < item.length; i++) {
-			if (
-				parseInt(item[i].style.top) + 100 == y &&
-				parseInt(item[i].style.left) == x
-			) {
-				return i;
-			}
-		}
-	} else if (y < 300 && random == 3) {
-		for (var i = 0; i < item.length; i++) {
-			if (
-				parseInt(item[i].style.top) - 100 == y &&
-				parseInt(item[i].style.left) == x
-			) {
-				return i;
-			}
-		}
-	} else {
-		return -1;
-	}
 }
-function moved(position) {
-	if (move(Y, X, 0) == position - 1) {
-		return true;
-	} else if (move(Y, X, 1) == position - 1) {
-		return true;
-	} else if (move(Y, X, 2) == position - 1) {
-		return true;
-	} else if (move(Y, X, 3) == position - 1) {
-		return true;
+
+
+
+// var sec = 0;
+// function timer ( val ) { 
+ 
+//     //break;
+  
+//   return  val > 9 ? val : "0" + val ; } 
+  
+//   setInterval( function(){
+    
+//     document.getElementById("seconds").innerHTML=timer(++sec%60);
+//     document.getElementById("minutes").innerHTML=timer(parseInt(sec/60,10));
+   
+// }, 1000);
+
+
+
+
+
+function start()
+{
+  var button = document.getElementById("scramble");
+  button.addEventListener( "click", startNewGame, false );
+  button.addEventListener( "click", countTimer, false);
+  // button.addEventListener( "click", bg, false );
+  textMoves = document.getElementById("moves");
+  table = document.getElementById("puzzle");
+
+  startNewGame();
+  // window.clearInterval(countTimer, 1000);
+ 
+  
+  countTimer();
+  
+  
+}
+
+
+var timerVar = setInterval(countTimer, 1000);
+var totalSeconds = 0;
+var hour, minute, seconds;
+function countTimer() {
+           ++totalSeconds;
+           hour = Math.floor(totalSeconds /3600);
+           minute = Math.floor((totalSeconds - hour*3600)/60);
+           seconds = totalSeconds - (hour*3600 + minute*60);
+           if(hour < 10)
+             hour = "0"+hour;
+           if(minute < 10)
+             minute = "0"+minute;
+           if(seconds < 10)
+             seconds = "0"+seconds;
+           document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+           
+           //reset time each timer each time shuffle is pressed
+           document.getElementById('scramble').addEventListener('click', () => {
+            totalSeconds = 0;
+            document.getElementById("timer").innerHTML = "0" + ":" + "0" + ":" + "0";
+         });
+        }
+
+
+function startNewGame()
+{
+  var bg = document.body.style.backgroundColor = "#0ec0c0";
+  var puzzlePieces = new Array();
+  var hasbeenUsed;
+  var randomNumber = 0;
+  var counter = 0;
+  moves = 0;
+  rows = document.getElementById("rows").value;
+  columns = document.getElementById("columns").value;
+  textMoves.innerHTML = moves;
+  // Create the proper board size.
+  arrayForBoard = new Array(rows);
+  
+  for (var i = 0; i < rows; i++)
+  {
+    arrayForBoard[i] = new Array(columns);
+  }
+  // Set up a temporary array for
+  // allocating unique numbers.
+  hasbeenUsed = new Array( rows * columns );
+  for (var i = 0; i < rows * columns; i++)
+  {
+    hasbeenUsed[i] = 0;
+  }
+ 
+  // Assign random numbers to the board.
+  for (var i = 0; i < rows * columns; i++)
+  {
+    randomNumber = Math.floor(Math.random()*rows * columns);
+    // If our random numer is unique, add it to the board.
+    if (hasbeenUsed[randomNumber] == 0) 
+    {
+      hasbeenUsed[randomNumber] = 1;
+      puzzlePieces.push(randomNumber);
+ 
+    }
+    else // Our number is not unique. Try again.
+    {
+      i--;
+    }
+    
+  }
+  
+  // Assign numbers to the game board.
+  counter = 0;
+  for (var i = 0; i < rows; i++)
+  {
+    for (var j = 0; j < columns; j++)
+    {
+      arrayForBoard[i][j] = puzzlePieces[counter];
+      
+      counter++;
+    }
+  }
+ 
+  Table();
+
+}
+
+
+
+
+
+    
+function Table()
+{
+  var outputString = "";
+  for (var i = 0; i < rows; i++)
+  {
+    outputString += "<tr>";
+    for (var j = 0; j < columns; j++)
+    {
+      if (arrayForBoard[i][j] == 0)
+      {
+	outputString += "<td class=\"emptyTile\"> </td>";
+      }
+      else
+      {
+	outputString += "<td class=\"tile\" onclick=\"moveThisTile(" + i + ", " + j + ")\">" + arrayForBoard[i][j] + "</td>";
+      }
+    } // end for (var j = 0; j < columns; j++)
+    outputString += "</tr>";
+  } // end for (var i = 0; i < rows; i++)
+  
+  table.innerHTML = outputString;
+}
+
+function moveThisTile( rowIndex, rowColumn)
+{
+  if (moveablePiece(rowIndex, rowColumn, "up") || moveablePiece(rowIndex, rowColumn, "down") || moveablePiece(rowIndex, rowColumn, "left") ||
+      moveablePiece(rowIndex, rowColumn, "right") )
+  {
+    incrementMoves();
+  }
+  
+ 
+  if (winsGame())
+  {
+
+    // setInterval(bg, 1000);
+    // function bg(){
+    document.body.style.backgroundImage = "url(./mario.png)";
+  // }
+    alert("Congratulations! You solved the puzzle in " + moves + " moves." + hour + ":" + minute + ":" + seconds);
+    var v = document.body.style.backgroundColor = "#0ec0c0";
+//  setInterval(bg, 1000);
+//  function bg(){
+//   document.body.style.backgroundImage = "url(./mario.png)";
+// }
+//clearInterval(timerVar);
+
+  
+    //clearInterval(timerVar);
+    // document.write(v);
+    // location.reload; 
+    startNewGame(); 
+    
+  }
+}
+
+function moveablePiece(rdirec, cdirec, direction)
+{
+  // The following variables an if else statements
+  // make the function work for all directions.
+  r = 0;
+  c = 0;
+  if (direction == "up")
+  {
+    r = -1;
+  }
+  else if (direction == "down")
+  {
+    r = 1;
+  }
+  else if (direction == "left")
+  {
+    c = -1;
+  }
+  else if (direction == "right")
+  {
+    c = 1;
+  }  
+  
+  // Check if the tile can be moved to the spot.
+  // If it can, move it and return true.
+  if (rdirec + r >= 0 && cdirec + c >= 0 && rdirec + r < rows && cdirec + c < columns
+  )
+    {
+      if ( arrayForBoard[rdirec + r][cdirec + c] == 0)
+      {
+        arrayForBoard[rdirec + r][cdirec + c] = arrayForBoard[rdirec][cdirec];
+        arrayForBoard[rdirec][cdirec] = 0;
+        Table();
+      return true;
+      }
+  }
+  return false; 
+}
+
+function winsGame()
+{
+  var counter = 1;
+  for (var i = 0; i < rows; i++)
+  {
+    for (var j = 0; j < columns; j++)
+    {
+      if (!(arrayForBoard[i][j] == counter))
+      {
+	if ( !(counter === rows * columns && arrayForBoard[i][j] === 0 ))
+	{
+	  return false;
 	}
+      }
+      counter++;
+    }
+  }
+ 
+  return true;
 }
-function swap_locations(position) {
-	var temp_position = item[position].style.top;
-	item[position].style.top = X;
-	X = temp_position;
-	temp_position = item[position].style.left;
-	item[position].style.left = Y;
-	Y = temp_position;
+
+function incrementMoves()
+{
+  moves++;
+  if (textMoves) // This is nessessary.
+  {
+    textMoves.innerHTML = moves;
+  }
 }
+
+window.addEventListener( "load", start, false ); // This event listener makes the function start() execute when the window opens. 
